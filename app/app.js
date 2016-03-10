afe43872636322a5eb11d098ce6b0cd39eec10c6
claus <claussoegaard@gmail.com>
2016-03-09 22:45:54 -0800

    done developing angular chord app

--- app/app.js
+++ app/app.js
@@ -1,12 +1,88 @@
+(function(){
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var app = angular.module('chordApp', []);
+
+app.controller('ChordController', function(){
+	this.chords = chords;
+
+	this.setResult = function(guess){
+		this.guessNo++;
+		this.guessIsCorrect = this.randomChord.name === guess;	
+	};
+
+	this.setRandomChord = function(){
+		var randomNumber = Math.floor(Math.random() * (14 - 1)) + 1;
+		this.randomChord = this.chords[randomNumber];
+	};
+
+	this.play = function(){
+		this.guessNo = 0;
+		this.guessIsCorrect = false;
+		this.setRandomChord();
+	};
+
+	this.play();
+	
+});
+
+var chords = [
+	{
+		name: "C", 
+		img: "img/gtrC.png"
+	}, 
+	{
+		name: "Cm",
+		img: "img/gtrCm.png"
+	},
+	{
+		name: "D", 
+		img: "img/gtrD.png"
+	}, 
+	{
+		name: "Dm",
+		img: "img/gtrDm.png"
+	},
+	{
+		name: "E", 
+		img: "img/gtrE.png"
+	}, 
+	{
+		name: "Em",
+		img: "img/gtrEm.png"
+	},
+	{
+		name: "F", 
+		img: "img/gtrF.png"
+	}, 
+	{
+		name: "Fm",
+		img: "img/gtrFm.png"
+	},
+	{
+		name: "G", 
+		img: "img/gtrG.png"
+	}, 
+	{
+		name: "Gm",
+		img: "img/gtrGm.png"
+	},
+	{
+		name: "A", 
+		img: "img/gtrA.png"
+	}, 
+	{
+		name: "Am",
+		img: "img/gtrAm.png"
+	},
+	{
+		name: "B", 
+		img: "img/gtrB.png"
+	}, 
+	{
+		name: "Bm",
+		img: "img/gtrBm.png"
+	},
+
+];
+})();
